bb00e5df08f7e6d6a8101a5880f967b0835feaa9
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 11:19:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ function login() {
   // Get input values
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-  if (username==''||password==''){
-    alert("All credentials should be filled")
-  }
 
   // Read CSV file
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       var rows = xhr.responseText.split("\n");
       for (var i = 0; i < rows.length; i++) {
         var columns = rows[i].split(",");
-        if (username == columns[0] && password == columns[1]) {
+        if (username === columns[0] && password === columns[1]) {
           // Login successful, redirect to desired page
           window.location.href = "https://example.com/dashboard";
           return;
@@ -26,3 +23,4 @@ function login() {
   xhr.open("GET", "users.csv", true);
   xhr.send();
 }
+
