36f231e8a46323da7e5f86dd97f2e6b43993e284
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 10:55:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function login() {
+  // Get input values
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username==''||password==''){
+    alert("All credentials should be filled")
+  }
+
+  // Read CSV file
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      var rows = xhr.responseText.split("\n");
+      for (var i = 0; i < rows.length; i++) {
+        var columns = rows[i].split(",");
+        if (username === columns[0] && password === columns[1]) {
+          // Login successful, redirect to desired page
+          window.location.href = "https://example.com/dashboard";
+          return;
+        }
+      }
+      // Login failed, display error message
+      alert("Invalid username or password.");
+    }
+  };
+  xhr.open("GET", "users.csv", true);
+  xhr.send();
+}
