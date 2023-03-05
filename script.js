06ad066fbca58fb15590879f14fbd361580ff6ca
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 13:23:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-Papa.parse('users.csv', {
-  download: true,
-  header: true,
-  complete: function(results) {
-    // Iterate over each row in the CSV file
-    for (var i = 0; i < results.data.length; i++) {
-      var row = results.data[i];
+function validateLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-      // Check if the entered username and password match this row in the CSV file
-      if (row.username === document.getElementById('username').value && row.password === document.getElementById('password').value) {
-        // If there's a match, allow the user to access the secured content
-        window.location.href = 'secured-content.html';
-        return;
+  // Read the CSV file and compare the username and password
+  // Use a library like PapaParse (https://www.papaparse.com/) to parse the CSV file
+  // Example code assuming PapaParse is used:
+  Papa.parse("users.csv", {
+    download: true,
+    complete: function(results) {
+      var users = results.data;
+      var found = false;
+      for (var i = 0; i < users.length; i++) {
+        if (users[i][0] == username && users[i][1] == password) {
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        alert("Login successful!");
+      } else {
+        alert("Invalid username or password");
       }
     }
+  });
+}
 
-    // If there's no match, display an error message
-    document.getElementById('error-message').innerText = 'Invalid username or password';
-  }
-});
