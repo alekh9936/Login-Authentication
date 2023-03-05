bc11019837db05a3f4a6b4a6cc44026271d1a29a
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 13:38:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 function validateLogin() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-
-  // Read the CSV file and compare the username and password
-  // Use a library like PapaParse (https://www.papaparse.com/) to parse the CSV file
-  // Example code assuming PapaParse is used:
-  Papa.parse("users.csv", {
-    download: true,
-    complete: function(results) {
-      var users = results.data;
-      var found = false;
-      for (var i = 0; i < users.length; i++) {
-        if (users[i][0] == username && users[i][1] == password) {
-          found = true;
-          break;
-        }
-      }
-      if (found) {
-        alert("Login successful!");
-      } else {
-        alert("Invalid username or password");
-      }
-    }
-  });
+if(username==''||password==''){
+  alert("All credentials should be filled")
 }
-
+  if(username=='Admin' && password=='Admin')||(username=='Student' && password=='Student'){
+    alert("Login Sucessful, redirecting)
+          }
+          else{
+          alert("Invalid Credentials")
+  }
+}
