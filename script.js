d9afc44735e1aa5a1f4f8e786f4b9860ff09ff57
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 13:54:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function login() {
+  // Get input values
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
-if(username==''||password==''){
-  alert("All credentials should be filled")
-}
-  if(username=='Admin' && password=='Admin')||(username=='Student' && password=='Student'){
-    alert("Login Sucessful, redirecting)
-          }
-          else{
-          alert("Invalid Credentials")
+  if (username==''||password==''){
+    alert("All credentials should be filled")
+  }
+  if (username=='Admin'&&password=='Admin'){
+    alert("login sucessful")
+    window.location.href = "https://example.com/dashboard";
+  }
+  else{
+    alert("login failed, credentials invalid")
   }
 }
