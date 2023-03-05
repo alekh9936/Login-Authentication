828758639f904915375ff6a7ad621b364884323f
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 14:36:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ function login() {
     alert("Credentials required")
   }
   if (username === "Admin" && password === "Admin"){
-    alert("Login successful, Redirecting");
+    alert("Login successful, Redirecting!");
     window.location.href = "https://example.com/dashboard";
   } 
+  if (username === "Test" && password === "Test"){
+    alert("Login successful, Redirecting!");
+    window.location.href = "https://example.com/dashboard";
+  }
+  if (username === "Student" && password === "Student"){
+    alert("Login successful, Redirecting!");
+    window.location.href = "https://example.com/dashboard";
+  }
+  if (username === "Faculty" && password === "Faculty"){
+    alert("Login successful, Redirecting!");
+    window.location.href = "https://example.com/dashboard";
+  }
+  if (username === "Bogus" && password === "Bogus"){
+    alert("Login successful, Redirecting!");
+    window.location.href = "https://example.com/dashboard";
+  }
   else {
     alert("Invalid username or password.");
   }
