57bdf6194a8b73a64e58c10260cf9b5c7a53ec5d
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 13:43:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function validateLogin() {
+function login() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 if(username==''||password==''){
