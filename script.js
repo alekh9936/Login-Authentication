d72de37b4ef0f4b9602581da758bc8d3c835ea8c
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 17:00:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 function login() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
+  
+if((username ==="Admin"&&password ==="Admin")||
+(username ==="Test"&&password ==="Test")||
+(username ==="Student"&&password ==="Student")||
+(username ==="Faculty"&&password ==="Faculty")||
+(username ==="Bogus"&&password ==="Bogus")){
+  alert("Login successful, Redirecting!")
+  window.location.href = "https://example.com/dashboard";
+}
+else if(username ===""||password ===""){
+  alert("Credentials required!")
+}
+else{
+  alert("Bad credentials!")
+}
 
-  // check if username and password are valid
-  if (username === "" || password === ""){
-    alert("Credentials required")
-  }
-  if (username === "Admin" && password === "Admin"){
-    alert("Login successful, Redirecting!");
-    window.location.href = "https://example.com/dashboard";
-  } 
-  if (username === "Test" && password === "Test"){
-    alert("Login successful, Redirecting!");
-    window.location.href = "https://example.com/dashboard";
-  }
-  if (username === "Student" && password === "Student"){
-    alert("Login successful, Redirecting!");
-    window.location.href = "https://example.com/dashboard";
-  }
-  if (username === "Faculty" && password === "Faculty"){
-    alert("Login successful, Redirecting!");
-    window.location.href = "https://example.com/dashboard";
-  }
-  if (username === "Bogus" && password === "Bogus"){
-    alert("Login successful, Redirecting!");
-    window.location.href = "https://example.com/dashboard";
-  }
-  else {
-    alert("Invalid username or password.");
-  }
 }
